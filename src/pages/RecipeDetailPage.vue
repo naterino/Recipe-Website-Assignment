d9db6f8ffd0508522dcc5bc955e6b2184933457e
Nathaniel Oliver <nateoliver@macbookpro.home>
2026-02-15 13:57:48 -0400

    Refined recipe detail page

--- src/pages/RecipeDetailPage.vue
+++ src/pages/RecipeDetailPage.vue
@@ -5,49 +5,60 @@
     <RouterLink to="/">Back to search</RouterLink>
   </template>
   <template v-else>
-    <h1>{{ recipe.title }}</h1>
-    <img
-      :src="`https://img.spoonacular.com/recipes/${recipe.id}-480x360.jpg`"
-      :srcset="`
+    <div class="flex flex-col md:flex-row gap-8 my-8">
+      <div class="md:min-w-1/2">
+        <img
+          :src="`https://img.spoonacular.com/recipes/${recipe.id}-480x360.jpg`"
+          :srcset="`
     https://img.spoonacular.com/recipes/${recipe.id}-240x150.jpg 240w,
     https://img.spoonacular.com/recipes/${recipe.id}-480x360.jpg 480w,
     https://img.spoonacular.com/recipes/${recipe.id}-636x393.jpg 636w
   `"
-      sizes="(max-width: 480px) 240px, (max-width: 768px) 480px, 636px"
-      :alt="recipe.title"
-    />
+          sizes="(max-width: 480px) 240px, (max-width: 768px) 480px, 636px"
+          :alt="recipe.title"
+        />
+      </div>
+      <div>
+        <h1 class="mb-4">{{ recipe.title }}</h1>
+        <div class="text-sm">
+          <div v-if="recipe.servings">{{ recipe.servings }} servings</div>
+          <div v-if="recipe.readyInMinutes">Ready in: {{ recipe.readyInMinutes }} minutes</div>
+          <ul class="health-information" v-if="healthLabels.length">
+            <li v-for="label in healthLabels" :key="label">{{ label }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <p v-html="recipe.summary" class="mb-8" />
+    <div class="flex flex-col md:flex-row gap-8">
+      <div class="md:w-140">
+        <h3>Ingredients</h3>
+        <button @click="useMetric = !useMetric">
+          {{ useMetric ? 'Metric' : 'Imperial' }}
+        </button>
+        <ul>
+          <IngredientItem
+            v-for="ing in recipe.extendedIngredients"
+            :key="ing.id"
+            :name="ing.name"
+            :measure="useMetric ? ing.measures.metric : ing.measures.us"
+          />
+        </ul>
+      </div>
 
-    <h2>Health Information</h2>
-    <ul class="health-information">
-      <li v-for="label in healthLabels" :key="label">{{ label }}</li>
-    </ul>
-
-    <h2>Ingredients</h2>
-    <button @click="useMetric = !useMetric">
-      {{ useMetric ? 'Metric' : 'Imperial' }}
-    </button>
-    <ul>
-      <IngredientItem
-        v-for="ing in recipe.extendedIngredients"
-        :key="ing.id"
-        :name="ing.name"
-        :measure="useMetric ? ing.measures.metric : ing.measures.us"
-      />
-    </ul>
-
-    <p v-html="recipe.summary" />
-    <h2>Cooking Steps</h2>
-    <div v-for="(group, i) in recipe.analyzedInstructions" :key="i">
-      <h3 v-if="group.name">{{ group.name }}</h3>
-      <ul>
-        <li v-for="step in group.steps" :key="step.number">
-          <span>{{ step.number }}:</span>{{ step.step }}
-        </li>
-      </ul>
+      <div>
+        <h2>Cooking Steps</h2>
+        <div v-for="(group, i) in recipe.analyzedInstructions" :key="i">
+          <h3 v-if="group.name">{{ group.name }}</h3>
+          <ul>
+            <li v-for="step in group.steps" :key="step.number">
+              <span>{{ step.number }}:</span>{{ step.step }}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </template>
-  Recipe Info:
-  <pre>{{ recipe }}</pre>
 </template>
 
 <script setup>
